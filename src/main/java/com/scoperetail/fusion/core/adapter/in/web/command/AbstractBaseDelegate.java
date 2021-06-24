b13e4adb63b5311c26e8a4a0930e6d07ba3f0d95
Javier Rivera Casanova <javier.casanova@scoperetail.com>
2021-06-24 10:23:15 -0500

    dedupe framework changes

--- src/main/java/com/scoperetail/fusion/core/adapter/in/web/command/AbstractBaseDelegate.java
+++ src/main/java/com/scoperetail/fusion/core/adapter/in/web/command/AbstractBaseDelegate.java
@@ -1,5 +1,31 @@
 package com.scoperetail.fusion.core.adapter.in.web.command;
 
+/*-
+ * *****
+ * fusion-core
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =====
+ */
+
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import org.springframework.http.HttpStatus;
@@ -10,12 +36,12 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 @Slf4j
 public abstract class AbstractBaseDelegate {
-  private final DuplicateCheckUseCase delegateUseCase;
+  private final DuplicateCheckUseCase duplicateCheckUseCase;
 
   protected HttpStatus doEvent(final String event, final Object domainEntity) {
     HttpStatus result = CONFLICT;
     try {
-      if (!delegateUseCase.isDuplicate(event, domainEntity)) {
+      if (!duplicateCheckUseCase.isDuplicate(event, domainEntity)) {
         result = doProcess(event, domainEntity);
       }
     } catch (final Exception e) {
@@ -35,6 +61,6 @@ public abstract class AbstractBaseDelegate {
     return result;
   }
 
-  protected abstract HttpStatus processEvent(Object domainEntity);
+  protected abstract HttpStatus processEvent(Object domainEntity) throws Exception;
 
 }
