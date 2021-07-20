36d435d386d31837c70fdecd9d29801bd4f6d50b
ravi-zadpide <ravi.zadpide@scoperetail.com>
2021-07-20 13:26:57 +0530

    MCS-4143 : Added logs in case of REST exception.

--- src/main/java/com/scoperetail/fusion/core/adapter/out/web/http/impl/PosterOutboundHttpAdapterImpl.java
+++ src/main/java/com/scoperetail/fusion/core/adapter/out/web/http/impl/PosterOutboundHttpAdapterImpl.java
@@ -13,10 +13,10 @@ package com.scoperetail.fusion.core.adapter.out.web.http.impl;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -47,25 +47,40 @@ import lombok.extern.slf4j.Slf4j;
 public class PosterOutboundHttpAdapterImpl implements PosterOutboundHttpAdapter {
 
   @Override
-  public void post(final String url, final String methodType, final String requestBody,
+  public void post(
+      final String url,
+      final String methodType,
+      final String requestBody,
       final Map<String, String> httpHeaders) {
     final HttpHeaders headers = new HttpHeaders();
     httpHeaders.entrySet().forEach(mapEntry -> headers.add(mapEntry.getKey(), mapEntry.getValue()));
     final HttpEntity<String> httpEntity = new HttpEntity<>(requestBody, headers);
 
-    HttpComponentsClientHttpRequestFactory clientHttpRequestFactory =
+    final HttpComponentsClientHttpRequestFactory clientHttpRequestFactory =
         new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build());
     final RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
     restTemplate.setErrorHandler(new DefaultResponseErrorHandler());
     restTemplate.setInterceptors(Collections.singletonList(new LoggingInterceptor()));
-    final ResponseEntity<String> exchange =
-        restTemplate.exchange(url, HttpMethod.valueOf(methodType), httpEntity, String.class);
-    log.trace("REST request sent to URL: {} and Response received is: {}", url, exchange);
+    try {
+      final ResponseEntity<String> exchange =
+          restTemplate.exchange(url, HttpMethod.valueOf(methodType), httpEntity, String.class);
+      log.trace("REST request sent to URL: {} and Response received is: {}", url, exchange);
+    } catch (final Exception e) {
+      log.error(
+          "An error occured while invoking REST endpoint \nURL: {} \nMethod:{} \nRequestBody:{} \nHeaders:{}",
+          url,
+          methodType,
+          requestBody,
+          headers);
+      throw e;
+    }
   }
 
   @Override
-  public void recover(RuntimeException exception, String message) {
-    log.error("On recover after retryPost failed. message: {}, Exception was: {} ", message,
+  public void recover(final RuntimeException exception, final String message) {
+    log.error(
+        "On recover after retryPost failed. message: {}, Exception was: {} ",
+        message,
         exception.getMessage());
   }
 }
