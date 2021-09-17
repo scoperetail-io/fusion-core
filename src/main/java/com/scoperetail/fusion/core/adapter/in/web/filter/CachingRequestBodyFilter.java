5e3aa88cbaf7625faa2cf0e6f67e8f4718acb9e7
ravi-zadpide <ravi.zadpide@scoperetail.com>
2021-09-17 13:21:13 +0530

    Code review fixes.

--- src/main/java/com/scoperetail/fusion/core/adapter/in/web/filter/CachingRequestBodyFilter.java
+++ src/main/java/com/scoperetail/fusion/core/adapter/in/web/filter/CachingRequestBodyFilter.java
@@ -12,10 +12,10 @@ package com.scoperetail.fusion.core.adapter.in.web.filter;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,7 +38,11 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 
 @Component
 public class CachingRequestBodyFilter extends GenericFilterBean {
-
+  //We need to read HttpRequest twice-
+  //1.During creation of audit
+  //2.During processing of HttpRequest.
+  //This was resulting in input stream closed exception.
+  //This class is used to avoid that exception.
   @Override
   public void doFilter(
       final ServletRequest servletRequest,
